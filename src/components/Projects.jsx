7c55557a8e656682d113b110d6fa734e6e72d873
unknown <rawanghoniama@gmail.com>
2025-11-16 14:41:38 +0200

    fix: json fetch path3

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,7 +9,7 @@ function Projects() {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    fetch(import.meta.env.BASE_URL + "data.json")
+    fetch("https://raw.githubusercontent.com/rawan809/portfolio/blob/main/public/data.json")
       .then((res) => res.json())
       .then((json) => setData(json))
       .catch((err) => console.error("Failed to load JSON:", err));
